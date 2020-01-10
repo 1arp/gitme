e6272352043307b449ad1074648729a039cd9fd7
1arp <dekhijaegi@gmail.com>
2020-01-10 16:26:38 +0530

    basic reffined -> remobved try catch
    Signed-off-by: 1arp <dekhijaegi@gmail.com>

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,25 +3,17 @@ const axios = require("axios").default;
 const route = express.Router();
 
 async function getRepos(username) {
-    try{
-        var response = await axios.get(`https://api.github.com/users/${username}/repos`);
-        var repos = await response.data;
-        console.log(response.headers['x-ratelimit-remaining']);
-        // var repos = await axios.get('https://jsonplaceholder.typicode.com/users');
-    }catch(err){
-        console.log(err)
-    }
+
+    var response = await axios.get(`https://api.github.com/users/${username}/repos`);
+    var repos = await response.data;
+    console.log(response.headers['x-ratelimit-remaining']);
 
     return repos;
 }
 
 route.get("/:id/repos", async (req, res) => {
-    try{
+    
     var repos = await getRepos(req.params.id);
-    }catch(err){
-        console.log(err)
-    }
-    //   console.log(repos)
     res.send(repos);
 });
 
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use('/',express.static(path.join(__dirname, "public")));
 
 app.use('/api',require('./routes/api'))
 
